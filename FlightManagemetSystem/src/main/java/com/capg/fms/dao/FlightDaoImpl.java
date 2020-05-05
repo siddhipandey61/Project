b2f5e7cba1c7811a8d75ef20e75a889042a24e4e
Ojaszw <60997216+Ojaszw@users.noreply.github.com>
2020-05-05 00:55:34 -0700

    Update FlightDaoImpl.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/dao/FlightDaoImpl.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/dao/FlightDaoImpl.java
@@ -1,42 +1,54 @@
 package com.capg.fms.dao;
 
 import java.util.List;
-
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
 import com.capg.fms.model.Flight;
 
+@Repository
 public class FlightDaoImpl implements FlightDao {
 
+	@PersistenceContext
+	EntityManager entityManager;	
+	
 	@Override
-	public Flight addFlight(Flight flight) {
-		// TODO Auto-generated method stub
-		return null;
+	public void  addFlight(Flight flight) {	
+		entityManager.persist(flight);
+		
 	}
 
 	@Override
-	public Flight updateFlight(Flight flight) {
-		// TODO Auto-generated method stub
-		return null;
+	public void updateFlight(Flight flight) {
+		entityManager.getTransaction().begin();
+		entityManager.merge(flight);
+		entityManager.getTransaction().commit();
 	}
 
 	@Override
 	public Flight retrieveFlight(long flightNumber) {
-		// TODO Auto-generated method stub
-		return null;
+		return entityManager.find(Flight.class, flightNumber);
 	}
 
 	@Override
 	public List<Flight> retrieveAllFlights() {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "select flight from Flight ";
+		TypedQuery<Flight> fs = entityManager.createQuery(query, Flight.class);
+		return fs.getResultList();	
 	}
 
 	@Override
-	public void deleteFlight(long fno) {
-		// TODO Auto-generated method stub
-		
+	public boolean deleteFlight(long flightNumber) {
+		Flight flight = entityManager.find(Flight.class, flightNumber);
+		if(flight != null)
+		{
+			entityManager.remove(flight);
+			return true;
+		}
+		else {
+		return false;	
+		}
 	}
-
-	
-
-
 }
+
