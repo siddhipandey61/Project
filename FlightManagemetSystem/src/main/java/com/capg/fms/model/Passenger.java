ce30168ec344593594b21b122e405db29502109f
virat483 <61452652+virat483@users.noreply.github.com>
2020-05-05 23:54:36 +0530

    Add files via upload

---
+++ FlightManagemetSystem/src/main/java/com/capg/fms/model/Passenger.java
@@ -0,0 +1,50 @@
+package com.capg.fms.model;
+
+public class Passenger {
+ 
+	private long pnrNumber;
+	private String passengerName;
+	private int passengerAge;
+	private long passengerUIN;
+	
+	public long getPnrNumber() {
+		return pnrNumber;
+	}
+
+	public void setPnrNumber(long pnrNumber) {
+		this.pnrNumber = pnrNumber;
+	}
+
+	public String getPassengerName() {
+		return passengerName;
+	}
+
+	public void setPassengerName(String passengerName) {
+		this.passengerName = passengerName;
+	}
+
+	public int getPassengerAge() {
+		return passengerAge;
+	}
+
+	public void setPassengerAge(int passengerAge) {
+		this.passengerAge = passengerAge;
+	}
+
+	public long getPassengerUIN() {
+		return passengerUIN;
+	}
+
+	public void setPassengerUIN(long passengerUIN) {
+		this.passengerUIN = passengerUIN;
+	}
+
+	public Passenger (long pnrNumber, String passengerName, int passengerAge, long passengerUIN) {
+		super();
+		
+		this.pnrNumber = pnrNumber;
+		this.passengerName = passengerName;
+		this.passengerAge = passengerAge;
+		this.passengerUIN = passengerUIN;
+	}
+}
