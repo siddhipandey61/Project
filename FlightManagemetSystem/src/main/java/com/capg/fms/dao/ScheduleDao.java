086203f63cf8f93bb6d4504edec92fbae4d33360
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-06 13:50:36 +0530

    Update ScheduleDao.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/dao/ScheduleDao.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/dao/ScheduleDao.java
@@ -7,7 +7,7 @@ import com.capg.fms.model.Schedule;
 public interface ScheduleDao {
 	public boolean addSchedule(Schedule schedule);
 	public Schedule retrieveSchedule(int scheduleid);
-	public List<Schedule> retrieveAllSchedule();
+	public List<Schedule> retrieveAllSchedule(int scheduleId);
 	public boolean updateSchedule(Schedule schedule);
 	public boolean deleteSchedule(int scheduleid);
 }
