4769d1bc3090240b516ea54348c9eaccff6ea224
mahima9801 <60997303+mahima9801@users.noreply.github.com>
2020-05-06 12:14:43 +0530

    Update ScheduledFlightsDaoImpl.java

--- FlightManagemetSystem/src/main/java/com/capg/fms/dao/ScheduledFlightsDaoImpl.java
+++ FlightManagemetSystem/src/main/java/com/capg/fms/dao/ScheduledFlightsDaoImpl.java
@@ -18,36 +18,38 @@ public class ScheduledFlightsDaoImpl implements ScheduledFlightsDao{
 	
 	// This module will add the flights scheduled in the ScheduledFlights.
 	@Override
-	public void addScheduledFlights(ScheduledFlights sflight) {
+	public boolean addScheduledFlights(ScheduledFlights sflight) {
 		entityManager.persist(sflight);
+		return true;
 	}
-
-	// This module will retrieve the flight scheduled in the ScheduledFlights.
+	
+	//This module will retrieve all the schedules based on the source and destination given by the customer.
+	@SuppressWarnings("unchecked")
 	@Override
-	public ScheduledFlights retrieveScheduledFlight(int serialno) {
-		return entityManager.find(ScheduledFlights.class, serialno);
+	public List<Schedule> retrieveScheduledFlights(String source, String destination) {
+		Query query = entityManager.createQuery("select s from Schedule s where Source="+source+"AND Destination="+destination);
+		return query.getResultList();
 	}
-
-	// This module will retrieve all flights scheduled in the ScheduledFlights.
-	@Override
-	public List<ScheduledFlights> retrieveAllScheduledFlights() {
-		String query = "select sflight from ScheduledFlights sflight";
-		TypedQuery<ScheduledFlights> q = entityManager.createQuery(query, ScheduledFlights.class);
-		return q.getResultList();
+	
+	@SuppressWarnings("unchecked")
+	public List<ScheduledFlights> retrieveAllScheduledFlights(int scheduleId){
+		Query query = entityManager.createQuery("select sf from ScheduledFlights sf where Schedule Id="+scheduleId);
+		return query.getResultList();
 	}
 	
 	// This module will update the flights scheduled in the ScheduledFlights.
 	@Override
-	public void updateScheduledFlights(ScheduledFlights sflight) {
+	public boolean updateScheduledFlights(ScheduledFlights sflight) {
 		entityManager.getTransaction().begin();
 		entityManager.merge(sflight);
 		entityManager.getTransaction().commit();
+		return true;
 	}
 	
 	// This module will delete the flights scheduled in the ScheduledFlights.
 	@Override
-	public boolean deleteScheduledFlights(int serialno) {
-		ScheduledFlights sflight =entityManager.find(ScheduledFlights.class, serialno);
+	public boolean deleteScheduledFlights(Schedule scheduleId) {
+		ScheduledFlights sflight =entityManager.find(ScheduledFlights.class, scheduleId);
 		if(sflight !=null)
 		{
 			entityManager.remove(sflight);
